3708914e8371d46244937cf99ac82bebcf3cb17a
markomitranic <marko.mitranic@me.com>
2016-04-23 16:49:51 +0200

    testing debugging

--- spellcheck.js
+++ spellcheck.js
@@ -13,41 +13,36 @@ function spellInit(selector, thresholdPercent) {
 	appendSpellWarning(selector);
 	var threshold = thresholdPercent;
 	var isTimeouting = false;
-	console.log('stigo sam doovde');
 
-	$(document).on('keydown', function() {
-		console.log('MARKO');
+	$(selector).on('keydown', function() {
+		if (isTimeouting === false) {
+		startTimeout(2000);
+
+		var content = tinymce.textContent;;
+		console.log(content);
+			jQuery.ajax({
+				method: 'POST',
+				url: 'spellchecker.php',
+					data: {
+						'article_text': content
+					},
+					success: function(data){
+						var response = data;
+						var response = JSON.parse(data);
+						var currentErrorPercent = calcPercent(response);
+						var isSubmitAllowed = checkResult(currentErrorPercent);
+						// Decide if should allow submit
+						if(isSubmitAllowed) {
+							$submit.removeAttr('disabled');
+						} else {
+							$submit.attr('disabled', 'disabled');
+						}	
+						// Write out the errors
+						$this.next().html(changeSpellWarning(isSubmitAllowed, currentErrorPercent, response.wrong_words));
+					}
+			});
+		}
 	});
-
-	$(document).on('keydown', function() {
-	if (isTimeouting === false) {
-	startTimeout(2000);
-
-	var content = selector.text();
-	console.log(content);
-		jQuery.ajax({
-			method: 'POST',
-			url: 'spellchecker.php',
-				data: {
-					'article_text': content
-				},
-				success: function(data){
-					var response = data;
-					var response = JSON.parse(data);
-					var currentErrorPercent = calcPercent(response);
-					var isSubmitAllowed = checkResult(currentErrorPercent);
-					// Decide if should allow submit
-					if(isSubmitAllowed) {
-						$submit.removeAttr('disabled');
-					} else {
-						$submit.attr('disabled', 'disabled');
-					}	
-					// Write out the errors
-					$this.next().html(changeSpellWarning(isSubmitAllowed, currentErrorPercent, response.wrong_words));
-				}
-		});
-	}
-});
 
 	function startTimeout(miliseconds) {
 		isTimeouting = true;
@@ -61,8 +56,6 @@ function spellInit(selector, thresholdPercent) {
 			'text': "There are no errors, you may post.",
 			'class': "spellWarning"
 		}).insertAfter(selector);
-	console.log('unutra sam zavrsio sve');
-
 	}
 
 	function changeSpellWarning(isSubmitAllowed, currentErrorPercent, wrongWords) {
