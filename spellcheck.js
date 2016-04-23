92e2397660e960a42906faa59bc7fa377117f08b
markomitranic <marko.mitranic@me.com>
2016-04-23 16:33:48 +0200

    testing debugging

--- spellcheck.js
+++ spellcheck.js
@@ -15,48 +15,33 @@ function spellInit(selector, thresholdPercent) {
 	var isTimeouting = false;
 	var customButton = button || false;
 
-	jQuery(document).on('keydown', selector, function(){
-		console.log('OTKRIO SE');
-		if (isTimeouting === false) {
-		startTimeout(2000);
-		var that = this;
-		var $this = jQuery(this);
-		$this.css('background-color','red');
+	if (isTimeouting === false) {
+	startTimeout(2000);
 
-		if (customButton === false) {
-			var $submit = $this.closest('form').find('input[type="submit"]');
-			var content = $this.val();
-		} else if (customButton === 'comments') {
-			var $submit = $this.parent('.es-comments-form').find('button');
-			var content = $this.val();
-		} else if (customButton === 'tinymce') {
-			var content = selector.text();
-		}
-
-		
-			jQuery.ajax({
-				method: 'POST',
-				url: 'spellchecker.php',
-					data: {
-						'article_text': content
-					},
-					success: function(data){
-						var response = data;
-						var response = JSON.parse(data);
-						var currentErrorPercent = calcPercent(response);
-						var isSubmitAllowed = checkResult(currentErrorPercent);
-						// Decide if should allow submit
-						if(isSubmitAllowed) {
-							$submit.removeAttr('disabled');
-						} else {
-							$submit.attr('disabled', 'disabled');
-						}	
-						// Write out the errors
-						$this.next().html(changeSpellWarning(isSubmitAllowed, currentErrorPercent, response.wrong_words));
-					}
-			});
-		}
-	});
+	var content = selector.text();
+	
+		jQuery.ajax({
+			method: 'POST',
+			url: 'spellchecker.php',
+				data: {
+					'article_text': content
+				},
+				success: function(data){
+					var response = data;
+					var response = JSON.parse(data);
+					var currentErrorPercent = calcPercent(response);
+					var isSubmitAllowed = checkResult(currentErrorPercent);
+					// Decide if should allow submit
+					if(isSubmitAllowed) {
+						$submit.removeAttr('disabled');
+					} else {
+						$submit.attr('disabled', 'disabled');
+					}	
+					// Write out the errors
+					$this.next().html(changeSpellWarning(isSubmitAllowed, currentErrorPercent, response.wrong_words));
+				}
+		});
+	}
 
 	function startTimeout(miliseconds) {
 		isTimeouting = true;
